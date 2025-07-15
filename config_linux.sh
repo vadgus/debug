f27b11a3c02c37265faf07fc68239bf97d43460b
Vadim <vadim.gusyatnikov@gmail.com>
2025-07-15 13:52:31 +0300

    Update config_linux.sh

--- config_linux.sh
+++ config_linux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# curl -fsSL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/vadgus/debug/refs/heads/main/config_linux.sh | sudo bash
+# echo -e '#!/bin/sh\ncurl -fsSL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/vadgus/debug/refs/heads/main/config_linux.sh | sudo bash\nreboot' > install.sh && chmod +x install.sh
+
 set -e
 
 # get real username
